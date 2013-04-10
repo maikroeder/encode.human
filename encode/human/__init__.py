1b15b5e77f347ae841b173ea3b64445c99b5c42a
maikroeder <maikroeder@gmail.com>
2013-04-10 21:02:18 +0200

    =refactor

--- encode/human/__init__.py
+++ encode/human/__init__.py
@@ -14,45 +14,31 @@ def counter():
 def debug(arg1, arg2):
 	import pdb; pdb.set_trace()
 
-def render_subtable(frame):
-    return ""
-    settings = {'index': ['Lab', 
-                          'Biological Replicate', 
-                          'Sample Internal Name', 
-                          'Read Type',
-                          'Insert Length',
-                          'Treatment'],
-                'meta': ['File URL', 
-                         'dateUnrestricted'],
-                'label': 'File View',
-                'column': 'File Type'}
-    context = {'settings':settings}
-    context['grouped'] = frame.groupby(settings['index'])
-    context['settings'] = settings
-    context['template_folder'] = 'template'
-    context['dimensions'] = settings['meta']
-    context['version'] = 'version'
-    context['column_headers'] = sorted(frame[settings['column']].unique())
-    return render_template(context, 'subtable')
-
 def level_1(self):
     self.variables = parse_variables(self.variables)
+    self.variables['dimensions'] = self.variables['dimensions'].split()
+    number = 0
+    for dimension_id in self.variables['dimensions']:
+        dimension = self.variables['dimension'][dimension_id.lower()]
+        dimension['number'] = number
+        number = number + 1
+    self.variables['grouped'] = self.variables['frame'].groupby(self.variables['dimensions'])
+
+def level_2(self):
+    self.variables = parse_variables(self.variables)
     self.variables['measure'] = lambda x:len(set(x))
-    self.variables['render_subtable'] = render_subtable
     self.variables['colours'] = self.variables['colours'].split()
     self.variables['dimensions'] = self.variables['dimensions'].split()
     self.variables['left_row_left'] = self.variables['left_row_left'].split()
     self.variables['left_row_right'] = self.variables['left_row_right'].split()
     self.variables['left_row'] = self.variables['left_row_left'] + self.variables['left_row_right'] 
     self.variables['left_rows'] = list(self.variables['frame'].groupby(self.variables['left_row']).ix.keys())
-    self.variables['grouped'] = self.variables['frame'].groupby(self.variables['dimensions'])
     url_template = self.variables['url_template']
     number = 0
     for dimension_id in self.variables['dimensions']:
         dimension = self.variables['dimension'][dimension_id.lower()]
         dimension['url'] = url_template % dimension['encode_id']
         unique_values = list(self.variables['frame'][dimension['id']].unique())
-        dimension['unique_values'] = unique_values
         dimension['number'] = number
         if dimension_id == self.variables['colour']:
             dimension['colour_mapping'] = {}
@@ -63,7 +49,7 @@ def level_1(self):
     self.variables['debug'] = debug
 
 
-def level_2(self):
+def level_3(self):
     self.variables = parse_variables(self.variables)
     self.variables['dimensions'] = self.variables['dimensions'].split()
     self.variables['index'] = self.variables['index'].split()
@@ -72,45 +58,20 @@ def level_2(self):
     number = 0
     for dimension_id in self.variables['dimensions']:
         dimension = self.variables['dimension'][dimension_id.lower()]
-        unique_values = list(self.variables['frame'][dimension['id']].unique())
-        dimension['unique_values'] = unique_values
         dimension['number'] = number
         number = number + 1
     self.variables['counter'] = counter()
     self.variables['debug'] = debug
     self.variables['template_folder'] = ''
 
-def level_3(self):
+def level_4(self):
     self.variables = parse_variables(self.variables)
     self.variables['dimensions'] = self.variables['dimensions'].split()
     number = 0
     for dimension_id in self.variables['dimensions']:
         dimension = self.variables['dimension'][dimension_id.lower()]
-        unique_values = list(self.variables['frame'][dimension['id']].unique())
-        dimension['unique_values'] = unique_values
         dimension['number'] = number
         number = number + 1
-
-def get_frame(self):
-    # Read the input file in ENCODE format
-    file_path = 'encode/human/apache_export/hg19_RNA_dashboard_files.txt'
-    frame = pd.DataFrame(parse(file_path), dtype=object)
-    frame['dateunrestricted'] = frame['dateUnrestricted']
-    frame['biologicalreplicate'] = frame['replicate']
-    frame['readtype'] = frame['readType']
-    frame['insertlength'] = frame['insertLength'].fillna('')
-    frame['fileview'] = frame['view']
-    frame['rnaextract'] = frame['rnaExtract']
-    frame['filetype'] = frame['type']
-    frame['fileurl'] = frame['File URL']
-    frame['treatment'] = frame['treatment'].fillna('')
-    frame['technology'] = frame['dataType'] + ' (' + frame['grant'] + ')'
-    frame['compartment'] = frame['localization'].fillna('cell').map({'cell':'Whole-cell'})
-    frame['labExpId'] = frame['labExpId'].map(lambda x: tuple(x.split(',')))
-    frame['sampleinternalname'] = frame['labExpId'].map(lambda x: ', '.join(x))
-    self.variables['frame'] = frame
-
-
 
 def get_frame(self):
     # Read the input file in ENCODE format
@@ -143,7 +104,7 @@ def get_frame(self):
             return tier[x]
         else:
             return '3'
-    frame['Tier'] = frame['cell'].map(to_tier)
+    frame['tier'] = frame['cell'].map(to_tier)
     frame['cell'] = frame['cell'].fillna('')
     frame['strain'] = frame['strain'].fillna('')
     frame['age'] = frame['age'].fillna('')
